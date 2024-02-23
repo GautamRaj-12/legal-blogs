68fbfbd6fe2ef3ad65e333ee373a005862a346b3
Gautam Raj <gautamraj996@gmail.com>
2024-02-23 15:56:34 +0530

    navbar component created and styled

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import logo from '../assets/legal-blogs-logo-light.png';
+const Navbar: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState<string>('block');
+  const [menuClose, setMenuClose] = useState<string>('hidden');
+  const [navLink, setNavLink] = useState<string>('hidden');
+
+  const handleOpenMenu = (): void => {
+    setMenuOpen('hidden');
+    setMenuClose('block');
+    setNavLink('flex');
+  };
+
+  const handleCloseMenu = (): void => {
+    setMenuClose('hidden');
+    setMenuOpen('block');
+    setNavLink('hidden');
+  };
+  return (
+    <>
+      <header className='px-4 md:py-2 py-4 bg-[#f8efea]'>
+        <nav className='grid grid-cols-12 gap-2 font-bold'>
+          <h1 className='col-span-3 '>
+            <img src={logo} alt='legal blogs by rohan logo' />
+          </h1>
+          <ul
+            className={`col-span-9 text-2xl md:flex ${navLink} md:flex-row flex-col justify-evenly items-center md:relative absolute md:top-auto top-0 md:right-auto right-0 bg-[#f8efea] md:h-auto h-screen md:w-auto w-1/2 animate-slide-from-right`}
+          >
+            <li>Home</li>
+            <li>Posts</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Login</li>
+            <li>Write</li>
+            <ul className='flex gap-5'>
+              <li>
+                <i className='fa-brands fa-x-twitter'></i>
+              </li>
+              <li>
+                <i className='fa-brands fa-linkedin-in'></i>
+              </li>
+              <li>
+                <i className='fa-brands fa-instagram'></i>
+              </li>
+            </ul>
+          </ul>
+          <div className='menu cursor-pointer absolute right-0 mr-2'>
+            <i
+              className={`fa-solid fa-bars text-4xl absolute right-0 md:hidden ${menuOpen}`}
+              onClick={handleOpenMenu}
+            ></i>
+            <i
+              className={`fa-solid fa-xmark text-4xl absolute right-0 ${menuClose}`}
+              onClick={handleCloseMenu}
+            ></i>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideFromRight: {
+          from: {
+            transform: 'translateX(100%)',
+            opacity: 0,
+          },
+          to: {
+            transform: 'translateX(0)',
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        'slide-from-right': 'slideFromRight 1s ease-in-out',
+      },
+    },
   },
   plugins: [],
 };
