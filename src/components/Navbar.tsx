351179485b850c23b98e5b96de8a2a22623c20c0
Gautam Raj <gautamraj996@gmail.com>
2024-02-24 16:05:33 +0530

    add routes footer about contact pages setup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import logo from '../assets/legal-blogs-logo-light.png';
+import { Link, NavLink } from 'react-router-dom';
 const Navbar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState<string>('block');
   const [menuClose, setMenuClose] = useState<string>('hidden');
@@ -21,17 +22,32 @@ const Navbar: React.FC = () => {
       <header className='px-4 md:py-2 py-4'>
         <nav className='grid grid-cols-12 gap-2 font-bold'>
           <h1 className='col-span-3 '>
-            <img src={logo} alt='legal blogs by rohan logo' />
+            <Link to='/'>
+              <img src={logo} alt='legal blogs by rohan logo' />
+            </Link>
           </h1>
           <ul
             className={`col-span-9 text-2xl md:flex ${navLink} md:flex-row flex-col justify-evenly items-center md:relative absolute md:top-auto top-0 md:right-auto right-0 md:bg-transparent bg-[--primary-bg-color] md:h-auto h-screen md:w-auto w-1/2 animate-slide-from-right`}
           >
-            <li>Home</li>
-            <li>Posts</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Login</li>
-            <li>Write</li>
+            <NavLink to='/'>
+              <li>Home</li>
+            </NavLink>
+            <NavLink to='#posts'>
+              <li>Posts</li>
+            </NavLink>
+            <NavLink to='/about'>
+              <li>About</li>
+            </NavLink>
+            <NavLink to='/contact'>
+              <li>Contact</li>
+            </NavLink>
+            <NavLink to='/Login'>
+              <li>Login</li>
+            </NavLink>
+            <NavLink to='/write'>
+              <li>Write</li>
+            </NavLink>
+
             <ul className='flex gap-5'>
               <li>
                 <i className='fa-brands fa-x-twitter'></i>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import Layout from './Layout.tsx';
+import About from './pages/About.tsx';
+import Home from './pages/Home.tsx';
+import Contact from './pages/Contact.tsx';
 
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home />,
+//       },
+//       {
+//         path: 'about',
+//         element: <About />,
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+    </Route>
+  )
+);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
