338b34e84211901109e03793051abc53572d7f7e
Gautam Raj <gautamraj996@gmail.com>
2024-05-18 14:46:14 +0530

    Add data from api in posts at home page

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -15,7 +15,10 @@ const Card = ({ postImg, postTitle, postDesc, postDate }: cardProps) => {
         />
         <div className="p-6 text-dark">
           <h1 className="text-headerThree">{postTitle}</h1>
-          <h2 className="text-paraTwo pt-3 font-roboto">{postDesc}</h2>
+          <h2
+            className="text-paraTwo pt-3 font-roboto"
+            dangerouslySetInnerHTML={{ __html: postDesc }}
+          ></h2>
           <div className="pt-14 flex justify-between items-center">
             <p className="text-smallText font-roboto uppercase">{postDate}</p>
             <button className="text-buttonText uppercase font-roboto py-3 px-6 rounded-full border border-dark">
--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,6 +1,30 @@
 import Card from "../Card/Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+interface postProps {
+  _id: string;
+  title: string;
+  desc: string;
+  coverImage: string;
+  createdAt: Date;
+}
 const Posts = () => {
+  const [posts, setPosts] = useState<postProps[]>([]);
+  useEffect(() => {
+    const fetchAllPosts = async () => {
+      try {
+        const res = await axios.get(
+          `${import.meta.env.VITE_API_URL}/posts/all-posts`
+        );
+        console.log(res?.data?.data);
+        setPosts(res?.data?.data.reverse());
+      } catch (error) {
+        console.log("Error");
+      }
+    };
+    fetchAllPosts();
+  }, []);
   return (
     <>
       <div className="flex flex-col items-center mt-4">
@@ -9,38 +33,27 @@ const Posts = () => {
         </h2>
       </div>
       <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 mt-4">
-        <Card
-          postImg={
-            "https://images.unsplash.com/photo-1715351885312-3ab5f7d85df1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
-          postTitle={"Title goes here"}
-          postDesc={"Description Goes here"}
-          postDate={"time goes here"}
-        />
-        <Card
-          postImg={
-            "https://images.unsplash.com/photo-1715351885312-3ab5f7d85df1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
-          postTitle={"Title goes here"}
-          postDesc={"Description Goes here"}
-          postDate={"time goes here"}
-        />
-        <Card
-          postImg={
-            "https://images.unsplash.com/photo-1715351885312-3ab5f7d85df1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
-          postTitle={"Title goes here"}
-          postDesc={"Description Goes here"}
-          postDate={"time goes here"}
-        />
-        <Card
-          postImg={
-            "https://images.unsplash.com/photo-1715351885312-3ab5f7d85df1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
-          postTitle={"Title goes here"}
-          postDesc={"Description Goes here"}
-          postDate={"time goes here"}
-        />
+        {posts.map((post) => (
+          <Card
+            key={post?._id}
+            postImg={post?.coverImage}
+            postTitle={
+              post?.title.length > 30
+                ? post.title.slice(0, 30) + "..."
+                : post?.title
+            }
+            postDesc={
+              post?.desc.length > 60
+                ? post.desc.slice(0, 60) + "..."
+                : post?.desc
+            }
+            postDate={new Date(post?.createdAt).toLocaleString("en-In", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          />
+        ))}
       </div>
       <button className="text-buttonText uppercase font-roboto py-3 px-6 mt-2 rounded-full bg-secondary flex mx-auto">
         Browse More
